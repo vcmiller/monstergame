0454da98f51f1094a4b66c5b31d3c0d550e61e75
Miller <vcmiller@wpi.edu>
2018-12-02 16:35:24 -0500

    asdf

---
+++ MonsterGame/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour {
+    public static ScoreCounter inst { get; private set; }
+    public int score { get; set; }
+
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        inst = this;
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "SCORE: " + score;
+	}
+}
--- MonsterGame/Assets/Scripts/ThePlayerDies.cs
+++ MonsterGame/Assets/Scripts/ThePlayerDies.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ThePlayerDies : MonoBehaviour {
+    public float dieTime = 4;
+
     void OnZeroHealth()
+    {
+        Invoke("Reload", dieTime);
+    }
+
+    void Reload()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
